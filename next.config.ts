ecde75a4681008286b95d725425303e02ea86953
Arian Mahdavi <amahdavi9978@gmail.com>
2025-08-12 22:28:11 +0330

    added env to next config.

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["d13080yemosbe2.cloudfront.net"],
   },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
 };
 
 export default nextConfig;
