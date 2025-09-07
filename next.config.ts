1eb147f3d37969648252fdcba7d30c85cbfd78fe
Arian Mahdavi <amahdavi9978@gmail.com>
2025-09-08 00:33:54 +0330

    fix base url

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,11 @@ const nextConfig: NextConfig = {
   },
   env: {
     NEXT_PUBLIC_API_URL:
-      process.env.NEXT_PUBLIC_API_URL || "http://13.60.167.192/api",
+      process.env.NEXT_PUBLIC_API_URL || "https://bulletgift.com/api",
     NEXT_PUBLIC_DASHBOARD_API_URL:
       process.env.NEXT_PUBLIC_DASHBOARD_API_URL ||
-      "http://13.60.167.192:82/api",
-    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL || "http://13.60.167.192",
+      "https://core.bulletgift.com/api",
+    NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL || "https://bulletgift.com",
   },
 };
 
