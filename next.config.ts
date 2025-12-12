0fa7fcdcc3d48096783c0694fab10a21b541b323
Arian Mahdavi <amahdavi9978@gmail.com>
2025-12-12 17:36:12 +0330

    fix: api dashboard base url

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,7 @@ const nextConfig: NextConfig = {
       process.env.NEXT_PUBLIC_API_URL || "https://bulletgift.com/api",
     NEXT_PUBLIC_DASHBOARD_API_URL:
       process.env.NEXT_PUBLIC_DASHBOARD_API_URL ||
-      "https://core.bulletgift.com/api",
+      "https://bulletgift.com/api",
     NEXT_PUBLIC_URL: process.env.NEXT_PUBLIC_URL || "https://bulletgift.com",
   },
 };
